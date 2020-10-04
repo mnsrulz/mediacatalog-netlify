afdcc474a209ed185a6b5af02092d009c4ff376c
Manish Rawat <manish_rawat@comcast.com>
2020-10-04 19:23:59 -0400

    added params

--- express/server.js
+++ express/server.js
@@ -7,12 +7,15 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 const cors = require('cors');
 
-router.get('/movies', async (req, res) => {
+router.get('/movies/:imdbId', async (req, res) => {
+  const imdbId = req.params.imdbId;
   const data = {
     data: [
       {
         title: "Test1",
-        source: "yt"
+        source: "yt",
+        imdbId,
+        params: req.params
       }
     ]
   }
@@ -26,6 +29,8 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+app.use('/pr1', (req, res) => res.json({ test: 123 }));
 
 module.exports = app;
 module.exports.handler = serverless(app);
