b5bf6584dfc97fc7db6d1ef6a7aafbc99e84fb78
Manish Rawat <manish_rawat@comcast.com>
2020-10-24 16:51:20 -0400

    fix for controller issue

--- src/app.ts
+++ src/app.ts
@@ -23,6 +23,10 @@ class App {
         this.app.use(cors());
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: false }));
+        this.app.use((req,res, next)=>{
+console.log('req comes in here...');
+next();
+        });
         // serving static files         
         // this.app.use(express.static('public'));
     }
