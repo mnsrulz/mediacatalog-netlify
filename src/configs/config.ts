412f44b158c636fedd84dc6949b24542f4bd5a37
mnsrulz <manishrawat4u@gmail.com>
2021-04-14 20:53:27 -0400

    system defined playlist bugfix while retrieving by id

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,5 +1,5 @@
 //Caution before checkin this to source code
 export default {
-    tmdbApiSecret: process.env.TMDB_API_SECRET || '',
-    mongoUri: process.env.MONGODB_URI || ''
+    tmdbApiSecret: process.env.TMDB_API_SECRET || "5c0c689d7e3f2a175015d5d171333312",
+    mongoUri: process.env.MONGODB_URI || 'mongodb+srv://testuser:BCO6HUx3UU5W58Gk@cluster0.9m1ac.mongodb.net/mediacatalog?retryWrites=true&w=majority'
 }
