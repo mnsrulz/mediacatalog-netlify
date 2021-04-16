2d0329fa3707e98371633e35ba31f174315bff53
mnsrulz <manishrawat4u@gmail.com>
2021-04-15 23:45:36 -0400

    reset the config id

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,7 +1,7 @@
 //Caution before checkin this to source code
 export default {
-    tmdbApiSecret: process.env.TMDB_API_SECRET || "5c0c689d7e3f2a175015d5d171333312",
-    driveFolderMovie: process.env.DRIVE_FOLDER_MOVIE,
-    driveFolderTv: process.env.DRIVE_FOLDER_TV,
-    mongoUri: process.env.MONGODB_URI || 'mongodb+srv://testuser:BCO6HUx3UU5W58Gk@cluster0.9m1ac.mongodb.net/mediacatalog?retryWrites=true&w=majority'
+    tmdbApiSecret: process.env.TMDB_API_SECRET || "",
+    driveFolderMovie: process.env.DRIVE_FOLDER_MOVIE || "",
+    driveFolderTv: process.env.DRIVE_FOLDER_TV || "",
+    mongoUri: process.env.MONGODB_URI || ''
 }
